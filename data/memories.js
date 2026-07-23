3bf49137e4b06cffa8299b8768c6690bb0631756
jetskiiix <hhecashh@gmail.com>
2026-07-22 20:32:08 -0500

    Add files via upload

---
+++ data/memories.js
@@ -0,0 +1,31 @@
+const memories = [
+
+{
+    image: "images/yappa.jpg",
+    title: "Yappa ❤️",
+    description:
+    "This little guy allowed us to bond together, and it's something I'm glad that happened."
+},
+
+{
+    image: "images/moon.jpg",
+    title: "The Moon 🌙",
+    description:
+    "Every night, I stare into the moon and get lost daydreaming about you."
+},
+
+{
+    image: "images/late.jpg",
+    title: "Late Night Talks ✨",
+    description:
+    "Every late night talk we have makes me remember how lucky I am to have someone like you."
+},
+
+{
+    image: "images/chapter.jpg",
+    title: "Another Chapter 📖",
+    description:
+    "I can't wait for the next adventure in our relationship ."
+}
+
+];
