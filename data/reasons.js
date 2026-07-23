2aaeba99815ec8d0694271b6cf9d0c977d98cfa1
jetskiiix <hhecashh@gmail.com>
2026-07-22 20:42:42 -0500

    Add files via upload

---
+++ data/reasons.js
@@ -0,0 +1,103 @@
+const reasons = [
+
+"I love your smile.",
+
+"I love your laugh.",
+
+"I love hearing your voice.",
+
+"I love how kind you are.",
+
+"I love how caring you are.",
+
+"I love how you always brighten my day.",
+
+"I love spending time with you.",
+
+"I love the little things you do.",
+
+"I love how safe I feel with you.",
+
+"I love your beautiful eyes.",
+
+"I love your hugs.",
+
+"I love how you make me feel understood.",
+
+"I love that I can be myself around you.",
+
+"I love how you make ordinary days special.",
+
+"I love every memory we've made together.",
+
+"I love how thoughtful you are.",
+
+"I love your sense of humor.",
+
+"I love how excited you get about things you enjoy.",
+
+"I love the way you make me smile without trying.",
+
+"I love your personality.",
+
+"I love how patient you are.",
+
+"I love your heart.",
+
+"I love how you support me.",
+
+"I love talking with you for hours.",
+
+"I love every adventure we share.",
+
+"I love how beautiful you are inside and out.",
+
+"I love your adorable habits.",
+
+"I love your confidence.",
+
+"I love your strength.",
+
+"I love how you make me want to become a better person.",
+
+"I love dreaming about our future together.",
+
+"I love the way you look at me.",
+
+"I love your random little jokes.",
+
+"I love your sleepy voice.",
+
+"I love every good morning message.",
+
+"I love every good night message.",
+
+"I love how much happiness you bring into my life.",
+
+"I love every second I spend with you.",
+
+"I love making memories with you.",
+
+"I love how much you mean to me.",
+
+"I love your compassion.",
+
+"I love your smile on difficult days.",
+
+"I love your positivity.",
+
+"I love how unique you are.",
+
+"I love your warmth.",
+
+"I love how amazing you make me feel.",
+
+"I love your curiosity.",
+
+"I love your honesty.",
+
+"I love your loyalty.",
+
+"I love simply being with you."
+
+];
